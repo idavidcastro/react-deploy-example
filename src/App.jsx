8bbd0ae7395aef6b5f558bfd2948470567a84911
idavidcastro <89281605+idavidcastro@users.noreply.github.com>
2024-04-25 12:43:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { tasks as data } from "./data/tasks";
+import { useState, useEffect } from "react";
+
+function HolaMundo() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </>
+  );
+}
+
+export default HolaMundo;
