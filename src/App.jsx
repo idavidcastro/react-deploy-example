09586d1cc374a28e45640ee1d54a06a4b8e83542
idavidcastro <89281605+idavidcastro@users.noreply.github.com>
2024-04-29 00:22:33 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
-import { tasks as data } from "./data/tasks";
-import { useState, useEffect } from "react";
 
 function HolaMundo() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask(task) {
-    setTasks([
-      ...tasks,
-      {
-        title: task.title,
-        id: tasks.length,
-        description: task.description,
-      },
-    ]);
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  }
-
   return (
-    <>
-      <TaskForm createTask={createTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
-    </>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
   );
 }
 
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskForm({ createTask }) {
+function TaskForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const { createTask } = useContext(TaskContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,20 +14,25 @@ function TaskForm({ createTask }) {
     setDescription("");
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Escribe tu tarea"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="escribe la descripción de la tarea"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      ></textarea>
-      <button>Guardar</button>
-    </form>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+        <input
+          placeholder="Escribe tu tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className="bg-slate-300 p-3 w-full mb-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="escribe la descripción de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
+        ></textarea>
+        <button className="bg-indigo-500 px-3 py-1 text-white ">Guardar</button>
+      </form>
+    </div>
   );
 }
 
